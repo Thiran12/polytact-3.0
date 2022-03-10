456ff1396398dc63fefe6a649a9b94c06fbff826
ESCG\520376 <520376@chrome.escg.ac.uk>
2022-03-10 16:14:05 +0000

    better camera and new ai

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,29 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class camera : MonoBehaviour
+{
+    public CinemachineFreeLook cinemachineFreeLook;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Pressed secondary button.");
+            cinemachineFreeLook.m_XAxis.m_MaxSpeed = 300;
+        }
+        else
+        {
+            cinemachineFreeLook.m_XAxis.m_MaxSpeed = 0;
+        }
+    }
+}
